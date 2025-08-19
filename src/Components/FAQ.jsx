f1c1e8eebd8c5f60dbdd8861547b63d7bb4a08da
souiiii <b423037@iiit-bh.ac.in>
2025-08-20 04:23:42 +0530

    minor changes

--- src/Components/FAQ.jsx
+++ src/Components/FAQ.jsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import styles from "./FAQ.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTurnDown } from "@fortawesome/free-solid-svg-icons";
+import { faTurnDown, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import gsap from "gsap";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function FAQ({ question, answer, index, display, setDisplay }) {
+  const tile = useRef(null);
 
-    useEffect(function(){
+  // useEffect(function(){
+  //     gsap.fromTo(tile.current, { height:0}, {y:0, opacity: 1, height: 'auto'})
 
-    },[display])
+  // },[display])
 
   return (
     <div className={styles.parent}>
@@ -15,10 +19,22 @@ export default function FAQ({ question, answer, index, display, setDisplay }) {
         className={styles.question}
         onClick={() => setDisplay(() => (index === display ? -1 : index))}
       >
-        <FontAwesomeIcon icon={faTurnDown} color="#00CE79" />
-        {question}
+        <div className={styles.quesSubDiv}>
+          <FontAwesomeIcon icon={faTurnDown} color="#00CE79" />
+          &nbsp;
+          {question}
+        </div>
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          
+          className={`${styles.arrow} ${display===index && styles.up}`}
+        />
       </div>
-      {index === display ? <div className={styles.answer}>{answer}</div> : null}
+      {index === display ? (
+        <div ref={tile} className={styles.answer}>
+          {answer}
+        </div>
+      ) : null}
     </div>
   );
 }
